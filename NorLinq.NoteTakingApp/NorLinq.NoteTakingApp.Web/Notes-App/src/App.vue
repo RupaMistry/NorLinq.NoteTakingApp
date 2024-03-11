0beef20fb5b4ae47281f96b63225a2d861451553
RupaMistry <mistryrupa7@gmail.com>
2024-03-11 08:09:57 +0400

    Added ListNotes component for displaying all notes.

--- NorLinq.NoteTakingApp/NorLinq.NoteTakingApp.Web/Notes-App/src/App.vue
+++ NorLinq.NoteTakingApp/NorLinq.NoteTakingApp.Web/Notes-App/src/App.vue
@@ -1,8 +1,26 @@
 <template>
   <header>
-    Hi
+    <div class="container">
+      Hi
+      <ListNotes :notes="notes" />
+    </div>
   </header>
 </template>
 
-<script setup lang="ts">
+<script lang="ts">
+
+import { defineComponent, ref } from "vue";
+import type Note from "./models/Note";
+import ListNotes from './components/notes/list.vue'
+
+export default defineComponent({
+  name: 'App',
+  components: { ListNotes },
+  setup() {
+    const notes = ref<Note[]>([]);
+
+    return { notes }
+  }
+})
+
 </script>
---
+++ NorLinq.NoteTakingApp/NorLinq.NoteTakingApp.Web/Notes-App/src/components/notes/list.vue
@@ -0,0 +1,47 @@
+<template>
+    <div class="container">
+        <ul class="list-group">
+            <div class="row">
+                <li v-for="note in  notes" :key="note.id" class="col-4">
+                    <br>
+                    <div>
+                        <div class="card">
+                            <div class="card-body text-center">
+                                <h5 class="card-title">{{ note.title }}</h5>
+                                <p class="card-text">
+                                    {{ note.comment }}
+                                </p>
+                                <a href="#" class="btn btn-primary" data-toggle="tooltip" data-placement="top"
+                                    title="Edit">Edit Note</a>
+                            </div>
+                            <div class="card-footer text-center text-body-secondary">
+                                {{ new Date(note.createdDate).toDateString() }}
+                            </div>
+                        </div>
+                    </div>
+                </li>
+            </div>
+        </ul>
+    </div>
+</template>
+
+<script lang="ts">
+import type Note from '@/models/Note';
+import { defineComponent, type PropType } from 'vue'
+
+export default defineComponent({
+    name: 'ListNotes',
+    components: {},
+    props: {
+        notes: {
+            required: true,
+            type: Array as PropType<Note[]>
+        }
+    },
+    setup() {
+
+    }
+})
+</script>
+
+<style scoped></style>
