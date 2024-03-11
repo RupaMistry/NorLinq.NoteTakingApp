6972e39a0c303d9ebe9e9e7ffa8611d94c8c3d86
RupaMistry <mistryrupa7@gmail.com>
2024-03-11 07:10:51 +0400

    Added Note entity and EF Core references

---
+++ NorLinq.NoteTakingApp/NorLinq.NoteTakingApp.Domain/Core/Entity.cs
@@ -0,0 +1,7 @@
+﻿namespace NorLinq.NoteTakingApp.Domain.Core
+{
+    public class Entity
+    {
+        public int ID { get; set; }
+    }
+}
---
+++ NorLinq.NoteTakingApp/NorLinq.NoteTakingApp.Domain/Note.cs
@@ -0,0 +1,20 @@
+﻿using NorLinq.NoteTakingApp.Domain.Core;
+using System.ComponentModel.DataAnnotations;
+
+namespace NorLinq.NoteTakingApp.Domain
+{
+    public class Note : Entity
+    {
+        public Note() { }
+
+        [Required]
+        public string Title { get; set; }
+
+        [Required]
+        public string Comment { get; set; }
+
+        public DateTime CreatedDate { get; set; }
+
+        //public string ColorCode { get; set; }
+    }
+}
