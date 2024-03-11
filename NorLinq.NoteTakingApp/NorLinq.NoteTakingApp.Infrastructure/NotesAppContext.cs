753ca639875a411d9cab0acf5b56485ccadaabd2
RupaMistry <mistryrupa7@gmail.com>
2024-03-11 07:18:21 +0400

    Added Notes table & EF Migrations.

---
+++ NorLinq.NoteTakingApp/NorLinq.NoteTakingApp.Infrastructure/NotesAppContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NorLinq.NoteTakingApp.Domain;
+
+namespace NorLinq.NoteTakingApp.Infrastructure
+{
+    public class NotesAppContext : DbContext
+    {
+        public NotesAppContext(DbContextOptions<NotesAppContext> options) : base(options) { }
+
+        public DbSet<Note> Notes { get; set; } = null!;
+    }
+}
